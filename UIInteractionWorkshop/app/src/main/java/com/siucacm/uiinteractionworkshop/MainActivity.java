f77fc5284d79fdeaaf6164e6a289465f489b0e97
jake32321 <jacob.reed95@siu.edu>
2017-04-18 13:57:52 -0500

    added methods to manipulate the UI

--- UIInteractionWorkshop/app/src/main/java/com/siucacm/uiinteractionworkshop/MainActivity.java
+++ UIInteractionWorkshop/app/src/main/java/com/siucacm/uiinteractionworkshop/MainActivity.java
@@ -1,13 +1,52 @@
 package com.siucacm.uiinteractionworkshop;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button greenButton, redButton, blueButton, setTextButton;
+    private EditText nameTextField, emailTextField;
+    private TextView nameText, emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greenButton = (Button) findViewById(R.id.buttonGreen);
+        redButton = (Button) findViewById(R.id.buttonRed);
+        blueButton = (Button) findViewById(R.id.buttonBlue);
+        setTextButton = (Button) findViewById(R.id.setText);
+
+        nameTextField = (EditText) findViewById(R.id.nameEditText);
+        emailTextField = (EditText) findViewById(R.id.emailEditText);
+
+        nameText = (TextView) findViewById(R.id.nameTextView);
+        emailText = (TextView) findViewById(R.id.emailTextView);
+
+
+        setTextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nameText.setText(nameTextField.getText());
+                emailText.setText(emailTextField.getText());
+            }
+        });
+
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!nameText.equals("") && !emailText.equals("")){
+                    nameText.setTextColor(Color.GREEN);
+                    emailText.setTextColor(Color.GREEN);
+                }
+            }
+        });
     }
 }
